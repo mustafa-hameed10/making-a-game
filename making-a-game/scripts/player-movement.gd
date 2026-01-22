e163ef6d58b2e1e35968dfad2071c36444d1a173
mustafa-hameed10 <hameedmustafa2020@gmail.com>
2026-01-22 17:01:27 +0500

    player movement

--- making-a-game/scripts/player-movement.gd
+++ making-a-game/scripts/player-movement.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
+@export_category("Stats")
+@export var speed = 200
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var move_direction = Vector2.ZERO
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	movement_loop()
+	
+func movement_loop():
+	move_direction.x = int(Input.is_action_pressed('d')) - int(Input.is_action_pressed('a'))
+	move_direction.y = int(Input.is_action_pressed('s')) - int(Input.is_action_pressed('w'))
+	var motion = move_direction.normalized() * speed
+	set_velocity(motion)
+	move_and_slide()
